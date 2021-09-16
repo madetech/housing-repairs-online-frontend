e8956e1e7fdc515e6f884825b6e00c120e725251
maysakanoni <maysa@madetech.com>
2021-09-16 13:35:37 +0100

    Add inital landing page content
    Co-authored-by: elena-vi <elena@madetech.com>
    Co-authored-by: bhavesh-patel <bhavesh.patel@madetech.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import './App.css';
-import { H1, Paragraph } from 'govuk-react';
+import {
+  H1,
+  H2,
+  Paragraph,
+  WarningText,
+  Details,
+  Button,
+  ButtonArrow,
+} from 'govuk-react';
 
 function App() {
   return (
     <div>
-      <H1>Lorem Impsum</H1>
+      <H1>Repairs Online</H1>
       <Paragraph>
-        Lorem `ipsum` **dolor** sit *amet* with [some link](https://google.com)
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum, enim quis placerat tristique, leo lacus gravida elit, ut placerat sem risus nec felis. Sed tincidunt tristique nisl, ut venenatis erat sagittis eget. Pellentesque id elit id felis venenatis pulvinar quis iaculis tellus. Nunc pellentesque bibendum tincidunt. Ut sagittis accumsan libero nec ornare. Curabitur interdum libero et nisl luctus, et mattis metus fermentum. Aenean libero nisl, faucibus non libero a, ultrices gravida lorem. Mauris a ipsum efficitur, lobortis ex eu, elementum urna. Praesent iaculis magna eget turpis sagittis, id sollicitudin est molestie.
+        We provide housing repairs for council tenants through this online
+        service.
+      </Paragraph>
+
+      <WarningText>
+        If you can smell gas, please call the gas emergency number on: 0800 111
+        999.
+      </WarningText>
+      <Details summary="What to do now if you smell gas">
+        <ol>
+          <li>Turn off the gas supply at the gas meter</li>
+          <li>Extinguish all sources of ignition</li>
+          <li>Do not smoke</li>
+          <li>Do not operate electrical light switches and power sockets</li>
+          <li>
+            Do not use the door entry system - if you need to let someone in, go
+            down to the entrance door to open it
+          </li>
+          <li>Open doors and windows to ventilate the area</li>
+          <li>
+            Contact National Grid on 0800 111 999 from outside the property
+          </li>
+        </ol>
+      </Details>
+      <Button icon={<ButtonArrow />}>Start now</Button>
+      <H2>Before you start</H2>
+      <Paragraph>
+        Reports can be made without signing up for a council account. If you
+        have an account you will be able to see your report there.
+      </Paragraph>
+      <H2>Emergency repairs</H2>
+      <Paragraph>
+        An emergency repair is when there&apos;s immediate danger to you or the
+        structure of the building. In an emergency we&apos;ll make the situation
+        safe; we may need to return another day to complete the full repair.
       </Paragraph>
       <Paragraph>
-        Morbi porttitor ornare convallis. Nulla ornare felis sed ex tempor molestie. Phasellus enim magna, accumsan a ultricies sit amet, sodales eget libero. Suspendisse vitae varius sem. Aenean et enim nec turpis consectetur euismod. Nulla facilisi. Nam fringilla nibh sed laoreet malesuada. Aenean ut lectus eget urna vulputate feugiat.
-      </Paragraph>
-      <Paragraph>
-        Aenean at lacus condimentum, dictum velit vel, interdum mi. Sed iaculis aliquam neque, sed finibus diam commodo sed. Fusce a lectus eget leo tempus lobortis eget nec ipsum. Sed scelerisque tortor in lectus sollicitudin sagittis. Proin sed nisi ac turpis lacinia sollicitudin non in leo. Etiam bibendum augue at orci auctor malesuada. Phasellus malesuada rhoncus lobortis. Aenean congue euismod turpis ac bibendum. Morbi sit amet lacus eleifend, maximus lorem vel, tincidunt magna. Donec tellus leo, commodo vitae libero vitae, pellentesque vestibulum purus. Vestibulum id lacus et odio aliquam fringilla id ac urna.
-      </Paragraph>
-      <Paragraph>
-        Nam sed ante tellus. Aliquam interdum convallis luctus. Phasellus ac justo dignissim, porta dolor et, dictum neque. Sed et pretium tortor. Cras sodales mi vel lacus consectetur, id sodales lectus volutpat. Suspendisse porttitor dolor pellentesque nibh scelerisque, nec varius sapien facilisis. Ut id metus facilisis, facilisis purus vitae, pellentesque orci. Integer gravida quam non sem consequat posuere. In hac habitasse platea dictumst. Ut sed purus eu neque dapibus convallis. Nulla quis hendrerit magna, at iaculis nulla. Curabitur lacinia luctus orci sit amet lobortis. Sed a arcu sit amet turpis feugiat ullamcorper.
-      </Paragraph>
-      <Paragraph>
-        Phasellus placerat augue a velit ultricies euismod. In dictum tempus nunc, ac consequat ante tincidunt non. Nulla malesuada libero at erat feugiat, vitae dapibus nisl molestie. Suspendisse pulvinar congue ex, vel finibus tellus suscipit sed. Phasellus ligula neque, pellentesque in convallis ut, aliquam in tortor. Sed maximus felis tortor, at volutpat neque pulvinar ut. Cras posuere ac dui nec facilisis. Etiam nunc lacus, suscipit non massa in, bibendum volutpat lorem. Pellentesque ullamcorper augue non ex ullamcorper euismod. Donec semper sapien eget rhoncus rutrum. Maecenas pellentesque massa eu vulputate semper. Quisque et tellus orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        All emergency repairs can be reported 24 hours a day, 7 days a week by
+        calling **0800 952 4444** or **020 7525 2600**. You may experience
+        longer waiting times due to a high volume of calls.
       </Paragraph>
     </div>
   );
