8dde655e8dcf6c5b514fc6a3dbc499f2fe172126
Bhavesh Patel <bhavesh.patel@madetech.com>
2021-09-16 14:04:29 +0100

    Added related items section
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: elena-vi <elena@madetech.com>

--- src/App.js
+++ src/App.js
@@ -2,60 +2,84 @@ import './App.css';
 import {
   H1,
   H2,
+  H3,
   Paragraph,
   WarningText,
   Details,
   Button,
   ButtonArrow,
+  UnorderedList,
+  ListItem,
+  Link,
+  RelatedItems,
+  GridRow,
+  GridCol,
 } from 'govuk-react';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div>
-      <H1>Repairs Online</H1>
-      <Paragraph>
-        We provide housing repairs for council tenants through this online
-        service.
-      </Paragraph>
+    <Fragment>
+      <GridRow>
+        <GridCol setWidth="two-thirds">
+          <H1>Report a repair</H1>
+          <Paragraph>
+            We provide housing repairs for council tenants through this online
+            service.
+          </Paragraph>
 
-      <WarningText>
-        If you can smell gas, please call the gas emergency number on: 0800 111
-        999.
-      </WarningText>
-      <Details summary="What to do now if you smell gas">
-        <ol>
-          <li>Turn off the gas supply at the gas meter</li>
-          <li>Extinguish all sources of ignition</li>
-          <li>Do not smoke</li>
-          <li>Do not operate electrical light switches and power sockets</li>
-          <li>
-            Do not use the door entry system - if you need to let someone in, go
-            down to the entrance door to open it
-          </li>
-          <li>Open doors and windows to ventilate the area</li>
-          <li>
-            Contact National Grid on 0800 111 999 from outside the property
-          </li>
-        </ol>
-      </Details>
-      <Button icon={<ButtonArrow />}>Start now</Button>
-      <H2>Before you start</H2>
-      <Paragraph>
-        Reports can be made without signing up for a council account. If you
-        have an account you will be able to see your report there.
-      </Paragraph>
-      <H2>Emergency repairs</H2>
-      <Paragraph>
-        An emergency repair is when there&apos;s immediate danger to you or the
-        structure of the building. In an emergency we&apos;ll make the situation
-        safe; we may need to return another day to complete the full repair.
-      </Paragraph>
-      <Paragraph>
-        All emergency repairs can be reported 24 hours a day, 7 days a week by
-        calling **0800 952 4444** or **020 7525 2600**. You may experience
-        longer waiting times due to a high volume of calls.
-      </Paragraph>
-    </div>
+          <WarningText>
+            If you can smell gas, please call the gas emergency number on: 0800 111
+            999.
+          </WarningText>
+          <Details summary="What to do now if you smell gas" className="govuk-!-margin-top-4">
+            <ol>
+              <li>Turn off the gas supply at the gas meter</li>
+              <li>Extinguish all sources of ignition</li>
+              <li>Do not smoke</li>
+              <li>Do not operate electrical light switches and power sockets</li>
+              <li>
+                Do not use the door entry system - if you need to let someone in, go
+                down to the entrance door to open it
+              </li>
+              <li>Open doors and windows to ventilate the area</li>
+              <li>
+                Contact National Grid on 0800 111 999 from outside the property
+              </li>
+            </ol>
+          </Details>
+          <br/>
+          <br/>
+          <Button icon={<ButtonArrow />} start='true' >Start now</Button>
+          <H2>Before you start</H2>
+          <Paragraph>
+            Reports can be made without signing up for a council account. If you
+            have an account you will be able to see your report there.
+          </Paragraph>
+          <H2>Emergency repairs</H2>
+          <Paragraph>
+            An emergency repair is when there&apos;s immediate danger to you or the
+            structure of the building. In an emergency we&apos;ll make the situation
+            safe; we may need to return another day to complete the full repair.
+          </Paragraph>
+          <Paragraph>
+            All emergency repairs can be reported 24 hours a day, 7 days a week by
+            calling **0800 952 4444** or **020 7525 2600**. You may experience
+            longer waiting times due to a high volume of calls.
+          </Paragraph>
+        </GridCol>
+        <GridCol setWidth="one-third">
+          <RelatedItems>
+            <H3>Example heading</H3>
+            <UnorderedList listStyleType="none">
+              <ListItem>
+                <Link href="https://example.com">Link A</Link>
+              </ListItem>
+            </UnorderedList>
+          </RelatedItems>
+        </GridCol>
+      </GridRow>
+    </Fragment>
   );
 }
 
