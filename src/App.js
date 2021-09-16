3974757a9d0231489358aeca0a7077eef995109b
el <elena@madetech.com>
2021-09-16 14:39:25 +0100

    add routing
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: Bhavesh-Patel <bhavesh.patel@madetech.com>

--- src/App.js
+++ src/App.js
@@ -1,87 +1,39 @@
 import './App.css';
+import LandingPage from './pages/landingPage'
 import {
-  H1,
-  H4,
-  H3,
-  Paragraph,
-  WarningText,
-  Details,
-  Button,
-  ButtonArrow,
-  UnorderedList,
-  OrderedList,
-  ListItem,
+  BrowserRouter as Router,
+  Switch,
   Link,
-  RelatedItems,
-  GridRow,
-  GridCol,
-} from 'govuk-react';
-import { Fragment } from 'react';
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <Fragment>
-      <GridRow>
-        <GridCol setWidth="two-thirds">
-          <H1>Report a repair</H1>
-          <Paragraph>
-            We provide housing repairs for council tenants through this online
-            service.
-          </Paragraph>
-          <WarningText>
-            If you can smell gas, please call the gas emergency number on: 0800 111
-            999.
-          </WarningText>
-          <Details summary="What to do now if you smell gas" className="govuk-!-margin-top-6">
-            <OrderedList>
-              <ListItem>Turn off the gas supply at the gas meter</ListItem>
-              <ListItem>Extinguish all sources of ignition</ListItem>
-              <ListItem>Do not smoke</ListItem>
-              <ListItem>Do not operate electrical light switches and power sockets</ListItem>
-              <ListItem>
-                Do not use the door entry system - if you need to let someone in, go
-                down to the entrance door to open it
-              </ListItem>
-              <ListItem>Open doors and windows to ventilate the area</ListItem>
-              <ListItem>
-                Contact National Grid on 0800 111 999 from outside the property
-              </ListItem>
-            </OrderedList>
-          </Details>
-          <Button icon={<ButtonArrow />} start='true' >Start now</Button>
-          <H3>Before you start</H3>
-          <Paragraph>
-            Reports can be made without signing up for a council account. If you
-            have an account you will be able to see your report there.
-          </Paragraph>
-          <H4>Emergency repairs</H4>
-          <Paragraph>
-            An emergency repair is when there&apos;s immediate danger to you or the
-            structure of the building. In an emergency we&apos;ll make the situation
-            safe; we may need to return another day to complete the full repair.
-          </Paragraph>
-          <Paragraph>
-            All emergency repairs can be reported 24 hours a day, 7 days a week by
-            calling **0800 952 4444** or **020 7525 2600**. You may experience
-            longer waiting times due to a high volume of calls.
-          </Paragraph>
-        </GridCol>
-        <GridCol setWidth="one-third">
-          <RelatedItems>
-            <H4>Related pages</H4>
-            <UnorderedList listStyleType="none">
-              <ListItem>
-                <Link href="#">Report a communal repair</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#">Fix it yourself videos</Link>
-              </ListItem>
-            </UnorderedList>
-          </RelatedItems>
-        </GridCol>
-      </GridRow>
-    </Fragment>
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <LandingPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
