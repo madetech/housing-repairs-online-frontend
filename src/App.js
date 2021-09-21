85eb06da6a44e73a6f1e362762c94af9a7487c48
el <elena@madetech.com>
2021-09-21 11:28:37 +0100

    change report repair to be a function, add routing to steps
    Co-authored-by: maysakanoni <maysa@madetech.com>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Route path="/report-repair">
           <ReportRepairPage />
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <LandingPage />
         </Route>
       </Switch>
--- src/components/reportRepair.js
+++ src/components/reportRepair.js
@@ -4,74 +4,88 @@ import Address from './reportRepair/address';
 import Confirmation from './reportRepair/confirmation';
 import TypeOfRepair from './reportRepair/typeOfRepair';
 import Emergency from './reportRepair/emergency';
+import {
+  Redirect,
+  Switch,
+  Route,
+  useRouteMatch
+} from 'react-router-dom';
 
-export default class Report extends Component {
-  state = {
+export default function Report() {
+
+  let state = {
     step: 1,
     postcode: '',
     address: '',
   };
-  prevStep = () => {
+  const prevStep = () => {
     const { prevStep, step } = this.state;
     const backstep = step - 1;
     this.setState({ prevStep: backstep > 0 ? backstep : 1});
     this.setState({ step: prevStep });
   };
-  nextStep = (skipTo) => {
-    const { step } = this.state;
-    this.setState({ prevStep: step });
+  const nextStep = (skipTo) => {
+    const { step } = state;
+    state.prevStep = step;
     if (skipTo) {
-      this.setState({ step: skipTo });
+      state.step = skipTo;
     } else {
-      this.setState({ step: step + 1 });
-    }
+      state.step = step + 1;
+    };
+    console.log(state);
   };
-  handleChange = (input, value) => {
-    this.setState({ [input]: value });
+  const handleChange = (input, value) => {
+    state[input] = value;
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.step !== 1) {
-      const back = this.prevStep
-      window.history.pushState(null, document.title, window.location.href);
-      window.addEventListener('popstate', function (event){
-        window.history.replaceState(null, document.title, window.location.href);
-        back();
-      });
-    }
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.step !== 1) {
+  //     const back = this.prevStep
+  //     window.history.pushState(null, document.title, window.location.href);
+  //     window.addEventListener('popstate', function (event){
+  //       window.history.replaceState(null, document.title, window.location.href);
+  //       back();
+  //     });
+  //   }
+  // }
 
-  render() {
-    const { step } = this.state;
-    const {postcode, address} = this.state;
-    const values = {postcode, address}
+  // const { step } = this.state;
+  const {postcode, address} = state;
+  const values = {postcode, address};
 
-    switch (step) {
-    case 1:
-      return (<TypeOfRepair
-        nextStep={this.nextStep}
-        handleChange={this.handleChange}
-        values={values}/>)
-    case 2:
-      return (<Postcode
-        nextStep={this.nextStep}
-        handleChange={this.handleChange}
-        values={values}/>)
-    case 3:
-      return (<Address
-        prevStep={this.prevStep}
-        nextStep={this.nextStep}
-        handleChange={this.handleChange}
-        values={values}/>)
-    case 4:
-      return (<Confirmation
-        values={values}/>)
-    // never forget the default case, otherwise VS code would be mad!
-    case 5:
-      return (<Emergency/>)
-    default:
-      // do nothing
-      return <div>nothing</div>
-    }
-  }
+  let { path, url } = useRouteMatch();
+
+  return (
+    <Switch>
+      <Route exact path={path}>
+        <Redirect to={`${path}/typeOfRepair`} />
+      </Route>
+      <Route path={`${path}/typeOfRepair`}>
+        <TypeOfRepair
+          nextStep={nextStep}
+          handleChange={handleChange}
+          values={values}/>
+      </Route>
+      <Route path={`${path}/postcode`}>
+        <Postcode
+          nextStep={nextStep}
+          handleChange={handleChange}
+          values={values}/>
+      </Route>
+      <Route path={`${path}/address`}>
+        <Address
+          prevStep={prevStep}
+          nextStep={nextStep}
+          handleChange={handleChange}
+          values={values}/>
+      </Route>
+      <Route path={`${path}/confirmation`}>
+        <Confirmation
+          values={values}/>
+      </Route>
+      <Route path={`${path}/emergency`}>
+        <Emergency/>
+      </Route>
+    </Switch>
+  )
 }
