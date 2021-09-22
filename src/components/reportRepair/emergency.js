48589f7d7ced0fb148023a318255fd3dbc0f9573
el <elena@madetech.com>
2021-09-22 13:45:26 +0100

    add emergency copy

--- src/components/reportRepair/emergency.js
+++ src/components/reportRepair/emergency.js
@@ -1,20 +1,24 @@
 import PropTypes from 'prop-types';
-import { H1,GridRow, GridCol } from 'govuk-react'
+import { H1,GridRow, GridCol, Paragraph } from 'govuk-react'
 
 
-const Emergency = ({handleChange, nextStep, values}) => {
+const Emergency = () => {
 
   return <GridRow>
-    <GridCol setWidth="one-third">
-      <H1> Emergency repair</H1>
+    <GridCol setWidth="two-thirds">
+      <H1>This repair may be an emergency</H1>
+      <Paragraph>
+        An emergency repair is when there&apos;s immediate danger to you or the
+        structure of the building. In an emergency we&apos;ll make the situation
+        safe; we may need to return another day to complete the full repair.
+      </Paragraph>
+      <Paragraph>
+        All emergency repairs can be reported 24 hours a day, 7 days a week by
+        calling **0800 952 4444** or **020 7525 2600**. You may experience longer
+        waiting times due to a high volume of calls.
+      </Paragraph>
     </GridCol>
   </GridRow>
 };
-
-Emergency.propTypes = {
-  nextStep: PropTypes.func,
-  values: PropTypes.object,
-  handleChange: PropTypes.func,
-}
 
 export default Emergency;
