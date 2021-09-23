95a4bb381e649c474d6d6973581f79cae2f2040f
el <elena@madetech.com>
2021-09-23 15:25:11 +0100

    edit routes

--- public/routes.js
+++ public/routes.js
@@ -1,19 +1,14 @@
 {
-    "routes": [
-        {
-            "route": "/"
-        }, {
-            "route": "/report-repair"
-        }, {
-            "route": "/report-repair/type"
-        }, {
-            "route": "/report-repair/postcode"
-        }, {
-            "route": "/report-repair/address"
-        }, {
-            "route": "/report-repair/confirmation"
-        }, {
-            "route": "/report-repair/emergency"
-        }
-    ]
+  "routes": [
+    {
+      "route": "/*",
+      "serve": "/index.html",
+      "statusCode": 200
+    }
+  ],
+  {
+    "navigationFallback": {
+      "rewrite": "index.html",
+    }
+  }
 }
