b32ad7ece78e4efcebfeb719a8b55564ac395075
maysakanoni <maysa@madetech.com>
2021-09-27 10:01:14 +0100

    Linting
    Co-authored-by: elena-vi <elena@madetech.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'plugin:editorconfig/all'
+  ],
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    "extends": [
-        "plugin:react/recommended",
-        "plugin:editorconfig/all"
-    ],
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "editorconfig"
-    ],
-    "rules": {
-        // suppress errors for missing 'import React' in files
-        "react/react-in-jsx-scope": "off",
-        "quotes": ["error", "single"]
-    }
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    'editorconfig'
+  ],
+  'rules': {
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    'quotes': ['error', 'single']
+  }
 };
