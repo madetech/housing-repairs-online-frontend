68126f4c667d09715bdb5c052399ee4e9c675da9
el <elena@madetech.com>
2021-10-11 15:48:30 +0100

    refactor radio fields

--- src/components/radioFieldSet.js
+++ src/components/radioFieldSet.js
@@ -1,9 +1,7 @@
 import PropTypes from 'prop-types';
 import {
-  Button, Fieldset,
-  MultiChoice, Radio
+  Button
 } from 'govuk-react'
-import { Field, Form } from 'react-final-form';
 import {Component} from 'react';
 
 class RadioFieldSet extends Component {
@@ -16,64 +14,60 @@ class RadioFieldSet extends Component {
     this.title = this.props.props.title;
     this.onSubmit = this.props.props.onSubmit;
     this.beforeButton = this.props.props.beforeButton;
+    this.state = {
+      error: null,
+      value: {[this.name]: this.options.find(o => o.checked)?.value}
+    };
   }
 
-  radioGroup = ({label, hint, options, input, meta}) => (
-    <div>
-      <MultiChoice label={label} hint={hint} meta={meta}>
-        {options.map(o => (
-          <Radio
-            key={o.value}
-            {...input}
-            value={o.value}
-            checked={o.checked}
-          >
-            {o.title}
-          </Radio>
-        ))}
-      </MultiChoice>
-    </div>
-  );
-
-  formSubmit = (params)=>{
-    if (Object.keys(params).length === 0 ) {
-      params[this.name] = this.options.find(o => o.checked).value;
-    }
-    this.onSubmit(params)
+  setValue(event) {
+    this.setState({
+      value: {[this.name]: event.target.value},
+      error: null
+    })
   };
 
-  required = (value) => {
-    if (value || this.options.find(o => o.checked)) {
-      return undefined
-    } else
-      return 'Required'
-  }
+  formSubmit = () => {
+    if (this.state.value[this.name]) {
+      this.onSubmit(this.state.value)
+    } else {
+      this.setState({error: 'Required'})
+    }
+  };
 
   render(){
     return (
-      <Form
-        onSubmit={this.formSubmit}
-        render={({handleSubmit}) => (
-          <form onSubmit={handleSubmit}>
-            <Fieldset>
-              <Fieldset.Legend size="XL" isPageHeading>{this.title}</Fieldset.Legend>
-              <Field
-                name={this.name}
-                label={this.label}
-                hint={this.hint}
-                component={this.radioGroup}
-                options={this.options}
-                validate={this.required}
-                type='radio'
-              />
-            </Fieldset>
-            <div className="govuk-!-margin-top-6">
-              {this.beforeButton}
-            </div>
-            <Button>Continue</Button>
-          </form>
-        )}
-      />
+      <div className={this.state.error ? 'govuk-form-group--error' : 'govuk-form-group'}>
+        <fieldset className="govuk-fieldset">
+          <legend
+            className="govuk-fieldset__legend govuk-fieldset__legend--l">
+            <h1 className="govuk-fieldset__heading">
+              {this.title}
+            </h1>
+          </legend>
+          <span id={`${this.name}-error`}
+            className="govuk-error-message">
+            {this.state.error}
+          </span>
+          <div className="govuk-radios" onChange={this.setValue.bind(this)}>
+            {this.options.map((o, i) => (
+              <div className="govuk-radios__item" key={i}>
+                <input className="govuk-radios__input"
+                  id={`${this.name}-${i}`} name={this.name}
+                  type="radio" value={o.value} defaultChecked={o.checked}/>
+                <label className="govuk-label govuk-radios__label"
+                  htmlFor={`${this.name}-${i}`}>
+                  {o.title}
+                </label>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+        <div className="govuk-!-margin-top-6">
+          {this.beforeButton}
+        </div>
+        <Button onClick={this.formSubmit}>Continue</Button>
+      </div>
     )
   }
 }
