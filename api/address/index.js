64362e2f8db34dada5948789e858ecec068d3b99
el <elena@madetech.com>
2021-11-16 17:08:22 +0000

    test frontend on azure

--- api/address/index.js
+++ api/address/index.js
@@ -1,10 +1,14 @@
-const {SearchPropertiesGateway} = require('../gateways');
+// const {SearchPropertiesGateway} = require('../gateways');
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
-  const result = await SearchPropertiesGateway(req.query.postcode);
+  // const result = await SearchPropertiesGateway(req.query.postcode);
 
   context.res = {
     status: 200,
-    body: result
+    body: [{
+      addressLine1: '1 Downing Street',
+      addressLine2: 'London',
+      postCode: req.query.postcode
+    }]
   };
 };
