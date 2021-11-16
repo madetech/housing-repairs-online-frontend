3e431660d49a2f94ac9be6c95e976ceb2073ffc5
maysakanoni <maysa@madetech.com>
2021-11-16 15:18:56 +0000

    match landing page to v3

--- pages/index.js
+++ pages/index.js
@@ -15,54 +15,23 @@ export default function Home() {
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-two-thirds">
         <h1>Request a repair</h1>
-        <h2>Before you start</h2>
-        <WarningText testid="landing-page-gas-warning">
-            If you suspect you have a gas leak, you must report it immediately
-            to the Gas Emergency Service on 0800 111 999 or via textphone
-            (minicom) on 0800 371 787
+        <h3>Before you start</h3>
+        <WarningText  testid="landing-page-report-limit-warning" className="govuk-!-margin-top-4">
+          This service can only be used to request one repair at a time to a council property.
         </WarningText>
-        <WarningText testid="landing-page-emergency-warning" className="govuk-!-margin-top-4">
-            For other emergency repairs, please see customer services {contactDetailsPageLink}.
-        </WarningText>
-        <Details summary="What is an emergency?" className="govuk-!-margin-top-6" testid="landing-page-emergency-prompt">
-          <div data-testid='landing-page-emergency-info'>
-            <p>
-                An emergency is defined as something which could cause danger to
-                someone’s health or safety or cause serious damage and destruction
-                to property.
-            </p>
-            <p>
-                Emergency Out of Hours Repairs, please call <strong>01522 534747</strong>
-            </p>
-            <p>
-                For emergencies in normal hours, please call <strong>01522 873333</strong>
-            </p>
-            <p>
-                Opening times:
-            </p>
-            <ul>
-              <li>Monday 9am - 5pm</li>
-              <li>Tuesday 9am - 5pm</li>
-              <li>Wednesday 9am - 4.30pm</li>
-              <li>Thursday 10am - 5pm</li>
-              <li>Friday 9am - 4.30pm</li>
-            </ul>
-          </div>
-        </Details>
         <p>
-            This service can only be used by <strong>council tenants</strong> to request a repair.
+          To request a repair for a <strong>leaseholder</strong>, please see our <TextLink href="https://www.lincoln.gov.uk/council-housing/council-leaseholders/3">leaseholder contact details page </TextLink>.
         </p>
         <p>
-            To request a repair for a <strong>communal area</strong>, please see customer
-            services {contactDetailsPageLink}.
+          To request a repair for a <strong>communal area</strong>, please see customer services contact <TextLink href="https://www.lincoln.gov.uk/contact-1/get-touch">details page</TextLink>.
         </p>
-        <Details summary="Which areas are communal?" className="govuk-!-margin-top-6" testid="landing-page-communal-prompt">
+        <Details summary="Which areas are communal?" className="govuk-!-margin-top-6" data-testid="landing-page-communal-prompt">
           <div data-testid='landing-page-communal-info'>
             <p>
-                Communal repairs are usually in areas that people share.
+              Communal repairs are usually in areas that people share.
             </p>
             <p>
-                They can include:
+              They can include:
             </p>
             <ul>
               <li>repairs to door entry systems</li>
@@ -71,6 +40,40 @@ export default function Home() {
               <li>glazing repairs to shared doors or stairway windows</li>
               <li>roof and gutter repairs</li>
               <li>structure and external walls to your block</li>
+            </ul>
+          </div>
+        </Details>
+        <br/>
+        <WarningText testid="landing-page-gas-warning" className="govuk-!-margin-top-4">
+            If you suspect you have a gas leak, you must report it immediately
+            to the Gas Emergency Service on 0800 111 999 or via textphone
+            (minicom) on 0800 371 787
+        </WarningText>
+        <WarningText testid="landing-page-emergency-warning"  className="govuk-!-margin-top-4">
+          For other emergency repairs, please see customer services <TextLink href="https://www.lincoln.gov.uk/contact-1/get-touch">contact details page</TextLink>.
+        </WarningText>
+        <Details summary="What is an emergency?" className="govuk-!-margin-top-6" testid="landing-page-emergency-prompt">
+          <div data-testid='landing-page-emergency-info'>
+            <p>
+              An emergency is defined as something which could cause danger to
+              someone’s health or safety or cause serious damage and destruction
+              to property.
+            </p>
+            <p>
+              Emergency Out of Hours Repairs, please call <strong>01522 534747</strong>
+            </p>
+            <p>
+              For emergencies in normal hours, please call <strong>01522 873333</strong>
+            </p>
+            <p>
+              Opening times:
+            </p>
+            <ul>
+              <li>Monday 9am - 5pm</li>
+              <li>Tuesday 9am - 5pm</li>
+              <li>Wednesday 9am - 4.30pm</li>
+              <li>Thursday 10am - 5pm</li>
+              <li>Friday 9am - 4.30pm</li>
             </ul>
           </div>
         </Details>
