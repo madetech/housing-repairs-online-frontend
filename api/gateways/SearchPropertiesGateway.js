30b93e479638e1fcb319ce217d6b65a427f828ea
Bhavesh Patel <bhavesh.patel@madetech.com>
2021-11-17 16:54:09 +0000

    Make address requests to repairs API
    Co-authored-by: elena-vi <elena@madetech.com>

--- api/gateways/SearchPropertiesGateway.js
+++ api/gateways/SearchPropertiesGateway.js
@@ -1,18 +1,29 @@
 require('dotenv').config()
-const fetch = require('node-fetch');
+const axios = require('axios');
 
-module.exports = async function(postcode) {
-  const response = await fetch(
-    `${process.env.REPAIRS_API}/addresses?postcode=${postcode}`,
-    {
-      headers: {
-        'X-API-Key': process.env.REPAIRS_API_KEY
+module.exports = async postcode => {
+  var result;
+  var identifier = process.env.REPAIRS_API_IDENTIFIER
+  var baseUrl = process.env.REPAIRS_API;
+  const axiosInstance = axios.create({
+      baseURL: baseUrl
+  })
+  result = await axiosInstance.post(`/authentication?identifier=${identifier}`)
+  .then(response => {
+      var jwt = response.data;
+      axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
+      return axiosInstance.get(`/addresses?postcode=${postcode}`);
+  })
+  .then(response => {
+      console.log(response);
+      return response.data;
+  })
+  .catch(error => {
+      console.log(error);
+      if (error.status >= 400) {
+        return new Error('Error searching');
       }
-    }
-  );
+  })
 
-  if (response.status >= 400) {
-    return new Error('Error searching');
-  }
-  return response.json();
-}
+  return result;
+};
