b9798486ef73f1323aedc713f0384fff2bdf7a57
el <elena@madetech.com>
2021-11-18 14:59:42 +0000

    add logging

--- api/address/index.js
+++ api/address/index.js
@@ -5,8 +5,9 @@ module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
   const results = await SearchPropertiesGateway(req.query.postcode);
 
+  context.log(results);
+
   context.res = {
-    status: 200,
     body: results
   };
 };
