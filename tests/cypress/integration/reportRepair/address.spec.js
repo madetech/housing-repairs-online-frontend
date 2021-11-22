380992862009e1b6c7bba5a89199de4fc1ae1f2a
maysakanoni <maysa@madetech.com>
2021-11-22 14:42:13 +0000

    add missing braces

--- tests/cypress/integration/reportRepair/address.spec.js
+++ tests/cypress/integration/reportRepair/address.spec.js
@@ -18,28 +18,27 @@ describe('address', () => {
   });
 
   describe('Content and interaction', () => {
-    beforeEach(()=>{
+    beforeEach(() => {
       setup_addresses_search(intercept_address_search);
     });
 
-  it('displays the label', () => {
-    cy.contains('Select an address');
+    it('displays the label', () => {
+      cy.contains('Select an address');
+    });
+
+    it('button displays correct text', () => {
+      cy.get('button').contains('Tell us where the problem is');
+    });
+
+    it('displays the question', () => {
+      cy.contains('Where is the repair located?');
+    });
+
+    it('contains a can\t find my address link', () => {
+      cy.contains('I can\'t find my address').click();
+      cy.url().should('include', '/report-repair/not-eligible');
+    });
   });
-
-  it('button displays correct text', () => {
-    cy.get('button').contains('Tell us where the problem is');
-  });
-
-  it('displays the question', () => {
-    cy.contains('Where is the repair located?');
-  });
-
-  it('contains a can\t find my address link',  () => {
-    cy.contains('I can\'t find my address').click();
-    cy.url().should('include', '/report-repair/not-eligible');
-  });
-
-
     context('When a user doesn\'t select anything', ()=>{
       it('an error should be shown',  () => {
         cy.get('button').click()
