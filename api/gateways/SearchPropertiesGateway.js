cb3ddfbcea61eca890b3d5bb41ba111ed4b0cc40
el <elena@madetech.com>
2022-01-13 11:38:37 +0000

    inject axios to helpers then inject helpers to search property gateway
    Co-authored-by: Bhavesh-Patel <bhavesh.patel@madetech.com>

--- api/gateways/SearchPropertiesGateway.js
+++ api/gateways/SearchPropertiesGateway.js
@@ -1,17 +1,18 @@
-const {makeGetRequest} = require('./helpers');
 
-module.exports = async postcode => {
-  var result;
-  result = await makeGetRequest({
-    url: `/addresses?postcode=${postcode}`
-  }).then(response => {
-    return response.data;
-  }).catch(error => {
-    console.error(error);
-    if (error.status >= 400) {
-      return new Error('Error searching');
-    }
-  })
+module.exports = makeGetRequest => {
+  return async postcode => {
+    var result;
+    result = await makeGetRequest({
+      url: `/addresses?postcode=${postcode}`
+    }).then(response => {
+      return response.data;
+    }).catch(error => {
+      console.error(error);
+      if (error.status >= 400) {
+        return new Error('Error searching');
+      }
+    })
 
-  return result;
-};
+    return result;
+  }};
+// execute
--- api/gateways/helpers.js
+++ api/gateways/helpers.js
@@ -1,36 +1,35 @@
 require('dotenv').config()
-const axios = require('axios');
 
-const makeGetRequest = ({url, params ={}}) =>{
-  var identifier = process.env.REPAIRS_API_IDENTIFIER
-  var baseUrl = process.env.REPAIRS_API_BASE_URL;
-  const axiosInstance = axios.create({
-    baseURL: baseUrl
-  })
-  return axiosInstance.post(`/authentication?identifier=${identifier}`)
-    .then(response => {
-      var jwt = response.data;
-      axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
-      return axiosInstance.get(url, {
-        params: params
-      });
-    })
-}
+module.exports = axios => {
+  return {
+    makeGetRequest: ({url, params ={}}) =>{
+      var identifier = process.env.REPAIRS_API_IDENTIFIER
+      var baseUrl = process.env.REPAIRS_API_BASE_URL;
+      const axiosInstance = axios.create({
+        baseURL: baseUrl
+      })
+      return axiosInstance.post(`/authentication?identifier=${identifier}`)
+        .then(response => {
+          var jwt = response.data;
+          axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
+          return axiosInstance.get(url, {
+            params: params
+          });
+        })
+    },
 
-const makePostRequest = ({url, body ={}}) =>{
-  var identifier = process.env.REPAIRS_API_IDENTIFIER
-  var baseUrl = process.env.REPAIRS_API_BASE_URL;
-  const axiosInstance = axios.create({
-    baseURL: baseUrl
-  })
-  return axiosInstance.post(`/authentication?identifier=${identifier}`)
-    .then(response => {
-      var jwt = response.data;
-      axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
-      return axiosInstance.post(url, body);
-    })
-}
-
-module.exports = {
-  makeGetRequest, makePostRequest
+    makePostRequest: ({url, body ={}}) =>{
+      var identifier = process.env.REPAIRS_API_IDENTIFIER
+      var baseUrl = process.env.REPAIRS_API_BASE_URL;
+      const axiosInstance = axios.create({
+        baseURL: baseUrl
+      })
+      return axiosInstance.post(`/authentication?identifier=${identifier}`)
+        .then(response => {
+          var jwt = response.data;
+          axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
+          return axiosInstance.post(url, body);
+        })
+    }
+  }
 }
