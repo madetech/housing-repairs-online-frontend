40f8b6913e2af4080edc6fd71965789a53f31e19
el <elena@madetech.com>
2022-01-14 14:14:34 +0000

    add deps to saveRepairGateway

--- api/gateways/index.js
+++ api/gateways/index.js
@@ -4,8 +4,10 @@ const apiRequester = require('./apiRequester')(axios);
 
 const searchPropertiesGateway = require('./SearchPropertiesGateway')(apiRequester.makeGetRequest);
 const availableAppointmentsGateway = require('./AvailableAppointmentsGateway')(apiRequester.makeGetRequest);
+const saveRepairGateway = require('./SaveRepairGateway')(apiRequester.makePostRequest);
 
 module.exports = {
   searchPropertiesGateway,
-  availableAppointmentsGateway
+  availableAppointmentsGateway,
+  saveRepairGateway
 };
