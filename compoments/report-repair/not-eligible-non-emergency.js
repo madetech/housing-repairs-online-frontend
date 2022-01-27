509ad10f89cc3d0f9b5b4f7f504d1334d1427370
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2022-01-27 15:32:30 +0000

    Update compoments/report-repair/not-eligible-non-emergency.js
    Co-authored-by: Bhavesh-Patel <Bhavesh-Patel@users.noreply.github.com>

--- compoments/report-repair/not-eligible-non-emergency.js
+++ compoments/report-repair/not-eligible-non-emergency.js
@@ -7,7 +7,7 @@ const NotEligibleNonEmergency = () => {
       <div className="govuk-grid-column-two-thirds">
         <h1 className='govuk-heading-xl'>The council may not be responsible for repairs at this property</h1>
         <p>
-          If you think the council is responsible for your property, please call <strong>01522 873 355</strong>.
+          If you think the council is responsible for your property, please call <strong>01522 873 333</strong>.
         </p>
         <p>
           If you are a leaseholder you can still report communal repairs on <strong>01522 873 333</strong>.
