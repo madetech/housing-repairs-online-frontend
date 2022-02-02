0569466fe4b4e6347b10aec260907ed17d5e4807
el <elena@madetech.com>
2022-02-02 14:17:51 +0000

    add outside door options

--- flow.js
+++ flow.js
@@ -34,8 +34,10 @@ class Flow {
       'repair-outside-problems': { prevSteps: 'repair-location', nextStep: [
         {condition: 'securityLights', nextStep: 'repair-description'},
         {condition: 'roof', nextStep: 'outside-roof-problems'},
+        {condition: 'door', nextStep: 'outside-door-problems'},
       ]},
       'outside-roof-problems':  { prevSteps: 'repair-outside-problems', nextStep: 'repair-description'},
+      'outside-door-problems':  { prevSteps: 'repair-outside-problems', nextStep: 'repair-description'},
       'repair-kitchen-problems': { prevStep: 'repair-location', nextStep: [
         {condition: 'cupboards', nextStep: 'repair-kitchen-cupboard-problems'},
         {condition: 'windows', nextStep: 'repair-window-problems'},
--- pages/report-repair/[route].js
+++ pages/report-repair/[route].js
@@ -270,6 +270,7 @@ function ReportRepair() {
           handleChange={handleChange}
           values={values}
           options = {[
+            { value: 'door', title: 'Door, including shed and outhouse'},
             { value: 'securityLights', title: 'Outdoor security lights'},
             { value: 'roof', title: 'Roof, including insulation and shed roof'}
           ]}
@@ -285,6 +286,20 @@ function ReportRepair() {
             { value: 'loftInsulation', title: 'Loft insulation'},
             { value: 'looseTiles', title: 'Loose tiles'},
             { value: 'flatRoofProblems', title: 'Problem with a flat roof'}
+          ]}
+        />
+      )
+    case 'outside-door-problems':
+      return (
+        <RepairProblemBestDescription
+          handleChange={handleChange}
+          values={values}
+          options = {[
+            { value: 'shedDoor', title: 'Shed door'},
+            { value: 'outhouseCupboardDoor', title: 'Outhouse cupboard door'},
+            { value: 'woodenBackDoor', title: 'Wooden back door'},
+            { value: 'UPVCBackDoor', title: 'UPVC back door'},
+            { value: 'frenchDoors', title: 'French doors'}
           ]}
         />
       )
@@ -522,6 +537,7 @@ export async function getStaticPaths() {
     {params: { route: 'repair-window-problems'} },
     {params: { route: 'repair-outside-problems'}},
     {params: { route: 'outside-roof-problems'}},
+    {params: { route: 'outside-door-problems'}},
     {params: { route: 'repair-description'} },
     {params: { route: 'repair-availability'} },
     {params: { route: 'smell-gas'} }
