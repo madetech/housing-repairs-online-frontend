1d3b68f4783f67aba5eff981831d2b0a3e65ecc0
Olly Swanson <olly.swanson95@gmail.com>
2022-09-05 12:46:35 +0100

    Update Next version and use standalone
    Updating the Next version allowed us to make use of `output: standalone`
    in `next.config.js` reducing the size of the Docker image from 1.2 GB to
    ~100 MB. Removed some unused packages and updated the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -27,11 +27,13 @@ ENV NEXT_TELEMETRY_DISABLED 1
 RUN addgroup --system --gid 1001 nodejs
 RUN adduser --system --uid 1001 nextjs
 
-COPY --from=builder /app/next.config.js ./
 COPY --from=builder /app/public ./public
-COPY --from=builder --chown=nextjs:nodejs /app/.next ./.next
-COPY --from=builder /app/node_modules ./node_modules
-COPY --from=builder /app/package.json ./package.json
+
+# Automatically leverage output traces to reduce image size
+# https://nextjs.org/docs/advanced-features/output-file-tracing
+COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
+COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
+COPY --from=builder --chown=nextjs:nodejs /app/public ./public
 
 USER nextjs
 
@@ -39,4 +41,4 @@ EXPOSE 3000
 
 ENV PORT 3000
 
-CMD ["node_modules/.bin/next", "start"]
+CMD ["node", "server.js"]
--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,51 @@ import WarningText from '../compoments/warningText';
 import Details from '../compoments/details';
 import TextLink from '../compoments/textLink';
 import React from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
 import ContactNumbers from '../compoments/contactNumbers';
 
 export default function Home() {
-
   return (
     <div className="govuk-grid-row govuk-body-m govuk-!-margin-top-7">
       <div className="govuk-grid-column-two-thirds">
         <h1 className={'govuk-heading-xl'}>Request a repair</h1>
         <h2 className={'govuk-heading-m'}>Before you start</h2>
-        <WarningText  testid="landing-page-report-limit-warning" className="govuk-!-margin-top-4">
-          This service can only be used to request one repair at a time to a council property.
+        <WarningText
+          testid="landing-page-report-limit-warning"
+          className="govuk-!-margin-top-4"
+        >
+          This service can only be used to request one repair at a time to a
+          council property.
         </WarningText>
         <p>
-          If you <strong>own the property</strong>, please see our <TextLink href="https://www.lincoln.gov.uk/council-housing/council-leaseholders/3" target={'_blank'}>leaseholder contact details page </TextLink>.
+          If you <strong>own the property</strong>, please see our{' '}
+          <TextLink
+            href="https://www.lincoln.gov.uk/council-housing/council-leaseholders/3"
+            target={'_blank'}
+          >
+            leaseholder contact details page{' '}
+          </TextLink>
+          .
         </p>
         <p>
-          To request a repair for a <strong>communal area</strong>, please see customer services contact <TextLink href="https://www.lincoln.gov.uk/contact-1/get-touch" target={'_blank'}>details page</TextLink>.
+          To request a repair for a <strong>communal area</strong>, please see
+          customer services contact{' '}
+          <TextLink
+            href="https://www.lincoln.gov.uk/contact-1/get-touch"
+            target={'_blank'}
+          >
+            details page
+          </TextLink>
+          .
         </p>
-        <Details summary="What is a communal area?" className="govuk-!-margin-top-6" data-testid="landing-page-communal-prompt">
-          <div data-testid='landing-page-communal-info'>
-            <p>
-              Communal repairs are usually in areas that people share.
-            </p>
-            <p className={'govuk-list govuk-list--bullet'}>
-              They can include:
-            </p>
+        <Details
+          summary="What is a communal area?"
+          className="govuk-!-margin-top-6"
+          data-testid="landing-page-communal-prompt"
+        >
+          <div data-testid="landing-page-communal-info">
+            <p>Communal repairs are usually in areas that people share.</p>
+            <p className={'govuk-list govuk-list--bullet'}>They can include:</p>
             <ul>
               <li>repairs to door entry systems</li>
               <li>lock repairs to communal doors</li>
@@ -40,24 +58,33 @@ export default function Home() {
           </div>
         </Details>
         <WarningText testid="landing-page-gas-warning" className="">
-            If you suspect you have a gas leak, you must report it immediately
-            to the Gas Emergency Service on 0800 111 999 or via textphone
-            (minicom) on 0800 371 787
+          If you suspect you have a gas leak, you must report it immediately to
+          the Gas Emergency Service on 0800 111 999 or via textphone (minicom)
+          on 0800 371 787
         </WarningText>
-        <WarningText testid="landing-page-emergency-warning"  className="">
-          For other emergency repairs, please see customer services <TextLink href="https://www.lincoln.gov.uk/contact-1/get-touch" target={'_blank'}>contact details page</TextLink>.
+        <WarningText testid="landing-page-emergency-warning" className="">
+          For other emergency repairs, please see customer services{' '}
+          <TextLink
+            href="https://www.lincoln.gov.uk/contact-1/get-touch"
+            target={'_blank'}
+          >
+            contact details page
+          </TextLink>
+          .
         </WarningText>
-        <Details summary="What is an emergency?" className="govuk-!-margin-top-6" testid="landing-page-emergency-prompt">
-          <div data-testid='landing-page-emergency-info'>
+        <Details
+          summary="What is an emergency?"
+          className="govuk-!-margin-top-6"
+          testid="landing-page-emergency-prompt"
+        >
+          <div data-testid="landing-page-emergency-info">
             <p>
               An emergency is defined as something which could cause danger to
               someone’s health or safety or cause serious damage and destruction
               to property.
             </p>
-            <ContactNumbers/>
-            <p>
-              Opening times:
-            </p>
+            <ContactNumbers />
+            <p>Opening times:</p>
             <ul>
               <li>Monday 9am - 5pm</li>
               <li>Tuesday 9am - 5pm</li>
@@ -68,11 +95,21 @@ export default function Home() {
           </div>
         </Details>
         <Link href="/report-repair/priority-list">
-          <a role="button" draggable="false"
+          <a
+            role="button"
+            draggable="false"
             className="govuk-button govuk-button--start"
-            data-module="govuk-button">
-              Start now
-            <svg className="govuk-button__start-icon" xmlns="http://www.w3.org/2000/svg" width="17.5" height="19" viewBox="0 0 33 40" aria-hidden="true" focusable="false">
+            data-module="govuk-button"
+          >
+            Start now
+            <svg
+              className="govuk-button__start-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              width="17.5"
+              height="19"
+              viewBox="0 0 33 40"
+              aria-hidden="true"
+            >
               <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
             </svg>
           </a>
