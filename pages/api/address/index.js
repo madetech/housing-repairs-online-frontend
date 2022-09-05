8981ec81b07012c7bc97e8cf5f11a18b7cb27cfb
Olly Swanson <olly.swanson95@gmail.com>
2022-09-05 14:21:13 +0100

    Remove Sentry configuration

--- pages/api/address/index.js
+++ pages/api/address/index.js
@@ -1,7 +1,7 @@
 const { searchPropertiesGateway } = require('../../../gateways');
 const { logger } = require('../../../helpers/log');
 
-// supports GET method
+// Supported methods = GET
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
