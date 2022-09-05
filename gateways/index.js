8fbf594e7fe95a0119ccb72103fc89e003d0a57b
Olly Swanson <olly.swanson95@gmail.com>
2022-09-05 14:21:13 +0100

    Remove Sentry configuration

--- gateways/index.js
+++ gateways/index.js
@@ -1,24 +1,19 @@
-require('dotenv').config()
-
 const axios = require('axios');
-
-const env = process.env.API_ENV || 'development';
-
-const sentryParams = {
-  dsn: process.env.SENTRY_DSN,
-  environment: env,
-  dryRun: env == 'development'
-};
 
 const apiRequester = require('./apiRequester')(axios);
 
-const searchPropertiesGateway = require('./SearchPropertiesGateway')(apiRequester.makeGetRequest);
-const availableAppointmentsGateway = require('./AvailableAppointmentsGateway')(apiRequester.makeGetRequest);
-const saveRepairGateway = require('./SaveRepairGateway')(apiRequester.makePostRequest);
+const searchPropertiesGateway = require('./SearchPropertiesGateway')(
+  apiRequester.makeGetRequest
+);
+const availableAppointmentsGateway = require('./AvailableAppointmentsGateway')(
+  apiRequester.makeGetRequest
+);
+const saveRepairGateway = require('./SaveRepairGateway')(
+  apiRequester.makePostRequest
+);
 
 module.exports = {
   searchPropertiesGateway,
   availableAppointmentsGateway,
   saveRepairGateway,
-  sentryParams
 };
--- pages/_error.js
+++ pages/_error.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import NextErrorComponent from 'next/error';
-
-import * as Sentry from '@sentry/nextjs';
+import { logger } from '../helpers/log';
 
 const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
   if (!hasGetInitialPropsRun && err) {
     // getInitialProps is not called in case of
     // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
     // err via _app.js so it can be captured
-    Sentry.captureException(err);
+    logger.captureException(err);
     // Flushing is not required in this case as it only happens on the client
   }
 
@@ -24,7 +23,7 @@ MyError.getInitialProps = async (context) => {
   // getInitialProps has run
   errorInitialProps.hasGetInitialPropsRun = true;
 
-  // Returning early because we don't want to log 404 errors to Sentry.
+  // Returning early because we don't want to log 404 errors.
   if (res?.statusCode === 404) {
     return errorInitialProps;
   }
@@ -43,22 +42,17 @@ MyError.getInitialProps = async (context) => {
   //    Boundaries: https://reactjs.org/docs/error-boundaries.html
 
   if (err) {
-    Sentry.captureException(err);
-
-    // Flushing before returning is necessary if deploying to Vercel, see
-    // https://vercel.com/docs/platform/limits#streaming-responses
-    await Sentry.flush(2000);
+    logger.captureException(err);
 
     return errorInitialProps;
   }
 
   // If this point is reached, getInitialProps was called without any
   // information about what the error might be. This is unexpected and may
-  // indicate a bug introduced in Next.js, so record it in Sentry
-  Sentry.captureException(
-    new Error(`_error.js getInitialProps missing data at path: ${asPath}`),
+  // indicate a bug introduced in Next.js, so record it
+  logger.captureException(
+    new Error(`_error.js getInitialProps missing data at path: ${asPath}`)
   );
-  await Sentry.flush(2000);
 
   return errorInitialProps;
 };
