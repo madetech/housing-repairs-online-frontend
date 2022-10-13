79ad1adff506dc15457cd957b7424780fbf7fa61
Olly Swanson <olly.swanson95@gmail.com>
2022-10-13 15:20:26 +0100

    Temporarily remove Lincoln logo/anchor

--- components/header.js
+++ components/header.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import Link from 'next/link'
-
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <>
-      <header
-        className="govuk-header"
-        role="banner"
-        data-module="govuk-header"
-      >
+      <header className="govuk-header" role="banner" data-module="govuk-header">
         <div className="govuk-header__container govuk-width-container">
           <div className="govuk-header__logo">
             {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-            <a href="https://www.lincoln.gov.uk/" className="govuk-header__logotype" target="_blank" rel="noopener noreferrer"/>
+            {/*
+            <a
+              href="https://www.lincoln.gov.uk/"
+              className="govuk-header__logotype"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            */}
           </div>
           <div className="govuk-header__content">
             <Link href="/">
-              <a
-                className="govuk-header__link govuk-header__link--service-name">
+              <a className="govuk-header__link govuk-header__link--service-name">
                 Housing Repairs Online
               </a>
             </Link>
