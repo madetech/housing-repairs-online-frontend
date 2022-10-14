01637752c7113ae2127e861b8570e482fad7f14a
Olly Swanson <olly.swanson95@gmail.com>
2022-10-14 11:45:58 +0100

    Removed some eslint rules
    They were erroneously reporting formatting issues and refusing to build
    the code.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,19 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'plugin:editorconfig/all',
-    'next/core-web-vitals'
-  ],
-  'parser': 'babel-eslint',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+  extends: ['plugin:react/recommended', 'next/core-web-vitals'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module'
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-    'editorconfig'
-  ],
-  'rules': {
-    // suppress errors for missing 'import React' in files
+  plugins: ['react'],
+  rules: {
     'react/react-in-jsx-scope': 'off',
-    'quotes': ['error', 'single']
-  }
+  },
 };
