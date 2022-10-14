5893c751dbb660a7b640b11d9cb7b27e08b31cb1
Olly Swanson <olly.swanson95@gmail.com>
2022-10-14 15:34:53 +0100

    Added GDS styling to the 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,24 @@
 import React from 'react';
-
-import Error from 'next/error'
+import { Layout } from '../components/layout';
+import { serviceName } from '../helpers/constants';
 
 export default function NotFound() {
-  return <Error statusCode={404} />
+  const pageHeading = 'Page not found';
+
+  return (
+    <Layout title={`${pageHeading} - ${serviceName}`}>
+      <main className="govuk-main-wrapper" id="main-content">
+        <div className="govuk-grid-row govuk-body-m">
+          <div className="govuk-grid-column-two-thirds">
+            <h1 className="govuk-heading-l">{pageHeading}</h1>
+            <p>If you typed the web address, check it is correct.</p>
+            <p>
+              If you pasted the web address, check you copied the entire
+              address.
+            </p>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
 }
