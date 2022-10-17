43833a12520bedbde2255b0085388a891bdd7cdd
Olly Swanson <olly.swanson95@gmail.com>
2022-10-17 16:09:53 +0100

    Add Redbridge branding (logo + favicon)

--- components/header.js
+++ components/header.js
@@ -8,14 +8,20 @@ export default function Header() {
         <div className="govuk-header__container govuk-width-container">
           <div className="govuk-header__logo">
             {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-            {/*
             <a
-              href="https://www.lincoln.gov.uk/"
-              className="govuk-header__logotype"
+              href="https://www.redbridge.gov.uk/"
               target="_blank"
               rel="noopener noreferrer"
-            />
-            */}
+              className="govuk-header__link govuk-header__link--homepage"
+            >
+              <span className="govuk-header__logotype">
+                <img
+                  alt="London Borough of Redbridge"
+                  src="/assets/images/redbridge-logo.png"
+                  height="46.5"
+                />
+              </span>
+            </a>
           </div>
           <div className="govuk-header__content">
             <Link href="/">
