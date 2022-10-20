25263cfb21ae54118a6f81c6664906e99f1c5215
Olly Swanson <olly.swanson95@gmail.com>
2022-10-20 12:45:04 +0100

    Update copy for unable-to-book
    The unable to book page is shown to users when either no appointments
    are found or when the tenant has, for example, no hot water in their
    kitchen. The message "unable to book" is confusing in these
    circumstances.

--- components/report-repair/unable-to-book.js
+++ components/report-repair/unable-to-book.js
@@ -4,10 +4,14 @@ const UnableToBook = () => {
   return (
     <div className="govuk-grid-row govuk-body-m">
       <div className="govuk-grid-column-two-thirds">
-        <h1 className="govuk-heading-xl">Your repair could not be booked</h1>
+        <h1 className="govuk-heading-xl">
+          You need to contact us to report this repair
+        </h1>
         <p>
-          To book your repair, please call <strong>01522 873333</strong>.
+          To book your repair, please call <strong>0208 498 8000</strong> or{' '}
+          <strong>0800 393994</strong>
         </p>
+        <p>Monday to Friday, 8am to 5pm (except public holidays)</p>
       </div>
     </div>
   );
