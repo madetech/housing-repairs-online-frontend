b27d44e2ea62cdc207d6d7cbb101ffd434122157
Olly Swanson <olly.swanson95@gmail.com>
2022-10-20 15:53:51 +0100

    Update copy for non-emergency pages

--- components/report-repair/not-eligible-communal-repairs.js
+++ components/report-repair/not-eligible-communal-repairs.js
@@ -3,28 +3,25 @@ import { serviceName } from '../../helpers/constants';
 
 const NotEligibleCommunalRepairs = () => {
   const title =
-    'For communal repairs, please call us during the office hours below';
+    'For communal repairs, please contact us using the details below';
   return (
     <div className="govuk-grid-row govuk-body-m">
       <div className="govuk-grid-column-two-thirds">
         <h1 className="govuk-heading-xl">{title}</h1>
-        <ul>
-          <li>Monday 8am - 5pm</li>
-          <li>Tuesday 8am - 5pm</li>
-          <li>Wednesday 8am - 5pm</li>
-          <li>Thursday 8am - 5pm</li>
-          <li>Friday 8am - 5pm</li>
-        </ul>
         <p>
           If you think the council is responsible for your property, please
-          call&nbsp;
-          <strong>01522 873333</strong>.
+          contact us using the details below.
         </p>
         <p>
-          If you are a leaseholder you can still report communal repairs
-          on&nbsp;
-          <strong>01522 873333</strong>.
+          If you are a leaseholder you can still report communal repairs using
+          the details below.
         </p>
+        <h2 className="govuk-heading-m">Repairs contact information</h2>
+        <ul class="govuk-list">
+          <li>Telephone: 0208 498 8000</li>
+          <li>Freephone: 0800 393994</li>
+          <li>Monday to Friday, 8am to 5pm (except public holidays)</li>
+        </ul>
       </div>
     </div>
   );
--- components/report-repair/unable-to-book.js
+++ components/report-repair/unable-to-book.js
@@ -7,11 +7,12 @@ const UnableToBook = () => {
         <h1 className="govuk-heading-xl">
           You need to contact us to report this repair
         </h1>
-        <p>
-          To book your repair, please call <strong>0208 498 8000</strong> or{' '}
-          <strong>0800 393994</strong>
-        </p>
-        <p>Monday to Friday, 8am to 5pm (except public holidays)</p>
+        <h2 className="govuk-heading-m">Repairs contact information</h2>
+        <ul class="govuk-list">
+          <li>Telephone: 0208 498 8000</li>
+          <li>Freephone: 0800 393994</li>
+          <li>Monday to Friday, 8am to 5pm (except public holidays)</li>
+        </ul>
       </div>
     </div>
   );
